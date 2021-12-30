882cc9773ae88b43e85b066195529a473357a253
root <root@LAPTOP-N5JE3Q4O.localdomain>
2021-12-30 00:42:10 +0000

    mudei o Machine está op bug continua e é bem estranho no clue on that shit ahah

--- Lab 4/exercise-4/machine_client.c
+++ Lab 4/exercise-4/machine_client.c
@@ -11,20 +11,30 @@
 #include <sys/un.h>
 
 int main()
-{
-    int sleep_delay;
+{   
+    message connection, movement, m_recieved;
+    struct sockaddr_un client_addr, server_addr;
+    int sleep_delay, n = 0;
+    socklen_t server_addr_size = sizeof(struct sockaddr_un);
     direction_t direction;
-    int n = 0;
-    message connection, movement;
+    
     //TODO_4
-
     int sock_fd = socket(AF_UNIX, SOCK_DGRAM, 0);
     if (sock_fd == -1)
     {
         perror("socket: ");
         exit(-1);
     }
-    printf(" socket created \n Ready to send\n");
+    client_addr.sun_family = AF_UNIX;
+    sprintf(client_addr.sun_path, "%s_%d", SOCK_ADDRESS, getpid());
+    unlink(client_addr.sun_path);
+    int err = bind(sock_fd, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    if (err == -1)
+    {
+        perror("bind");
+        exit(-1);
+    }
+    printf("Socket created \nReady to send\nReady to recieve\n");
 
     //TODO_5
     // machine will generate a random character and send to the server making the connection
@@ -32,7 +42,6 @@ int main()
     connection.msg_type = 0;
     movement.ch = connection.ch;
 
-    struct sockaddr_un server_addr;
     server_addr.sun_family = AF_UNIX;
     strcpy(server_addr.sun_path, SOCK_ADDRESS);
     sendto(sock_fd, &connection, sizeof(connection), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
@@ -50,19 +59,19 @@ int main()
         switch (direction)
         {
         case LEFT:
-            printf("%d Going Left   ", n);
+            //printf("%d Going Left   ", n);
             movement.direction = LEFT;
             break;
         case RIGHT:
-            printf("%d Going Right   ", n);
+            //printf("%d Going Right   ", n);
             movement.direction = RIGHT;
             break;
         case DOWN:
-            printf("%d Going Down   ", n);
+            //printf("%d Going Down   ", n);
             movement.direction = DOWN;
             break;
         case UP:
-            printf("%d Going Up    ", n);
+            //printf("%d Going Up    ", n);
             movement.direction = UP;
             break;
         }
@@ -71,7 +80,11 @@ int main()
 
         //TODO_10
         sendto(sock_fd, &movement, sizeof(movement), 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        recvfrom(sock_fd, &m_recieved, sizeof(m_recieved), 0, (struct sockaddr *)&server_addr, &server_addr_size);
+        if (m_recieved.msg_type == 1)
+            flash();
     }
 
     return 0;
+    close(sock_fd);
 }
